91450591648729a1225d68dfbaa7dcad833356d6
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-07-19 12:15:25 +0200

    Hide navigation header

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import LogIn from './screens/LogIn';
+import { LogIn } from './screens/LogIn';
 import Home from './screens/Home';
 
 const Stack = createNativeStackNavigator();
@@ -12,18 +11,17 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="LogIn" component={LogIn} options={{headerShown: false}} />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="LogIn"
+          component={LogIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
