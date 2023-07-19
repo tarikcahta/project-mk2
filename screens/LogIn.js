1ec9487c055bb636e0bc50da91f14b1c922f5baa
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-07-19 12:17:32 +0200

    Refactor

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -4,29 +4,37 @@ import LogInInput from '../components/LogInInput';
 import SocialMediaBtns from '../components/SocialMediaBtns';
 import SocialLoginScreen from 'react-native-social-login-screen';
 
-export default function LogIn() {
+export const LogIn = ({ navigation }) => {
+  const handleLogIn = () => {
+    navigation.navigate('Home');
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.headerPosition}>
         <View>
-          <Text style={styles.headerTextStyle}>Log In</Text>
+          <Text style={[styles.headerTextStyle, styles.textStyle]}>Log In</Text>
         </View>
       </View>
       <View style={styles.middleContentPosition}>
         <LogInInput placeholder="Username" />
         <LogInInput placeholder="Password" />
 
-        <TouchableOpacity style={styles.buttonStyle}>
-          <Text style={styles.buttonTextStyle}>Log in</Text>
+        <TouchableOpacity onPress={handleLogIn} style={styles.buttonStyle}>
+          <Text style={[styles.buttonTextStyle, styles.textStyle]}>Log in</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.footerContentPosition}>
         <View style={styles.hintTextPosition}>
-          <Text style={styles.hintText}>Or Sign in with </Text>
+          <Text style={[styles.hintText, styles.textStyle]}>
+            Or Sign in with{' '}
+          </Text>
         </View>
         <SocialMediaBtns />
         <View style={styles.signUpStyle}>
-          <Text style={styles.signUpTextStyle}>Don't have an account?</Text>
+          <Text style={[styles.signUpTextStyle, styles.textStyle]}>
+            Don't have an account?
+          </Text>
           <TouchableOpacity>
             <Text style={styles.signUpButtonStyle}>Sign up</Text>
           </TouchableOpacity>
@@ -34,4 +42,4 @@ export default function LogIn() {
       </View>
     </View>
   );
-}
+};
