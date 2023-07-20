f0da869aac7c7487c23eba8c34a993d9ef91b939
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-07-20 12:45:09 +0200

    Add card component styles

---
+++ components/styles/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export const stylesComponents = StyleSheet.create({
+  cardStyle: {
+    backgroundColor: '#c3c4ce',
+    borderRadius: 35,
+    marginVertical: 25,
+    width: '95%',
+    height: 150,
+  },
+  gradient: {
+    flex: 1,
+    borderRadius: 35,
+  },
+  tempStyle: {
+    marginLeft: 80,
+    marginTop: 20,
+    fontSize: 50,
+  },
+  decimalTempStyle: {
+    fontSize: 25,
+    // marginTop: 20,
+    marginLeft: 0,
+  },
+});
