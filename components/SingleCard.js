de333b339fbd14082cc927241b28f0055d1e56e3
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-07-23 10:22:34 +0200

    Modify card component to display temperature

--- components/SingleCard.js
+++ components/SingleCard.js
@@ -2,19 +2,19 @@ import { View, Text } from 'react-native';
 import { stylesComponents } from './styles/styles';
 import { LinearGradient } from 'expo-linear-gradient';
 
-export const SingleCard = () => {
+export const SingleCard = ({ value }) => {
+  const [integer, decimal] = value.toString().split('.');
   return (
     <View style={stylesComponents.cardStyle}>
       <LinearGradient
         colors={['#c5f7d1', '#dffbca', '#f0f7dc']}
         style={stylesComponents.gradient}
       >
-        <View styles={{ flexDirection: 'row', width: '100%' }}>
-          <View>
-            <Text style={stylesComponents.tempStyle}>17 °</Text>
-          </View>
-          <View>
-            {/* <Text style={stylesComponents.decimalTempStyle}>.0</Text> */}
+        <View style={stylesComponents.container}>
+          <Text style={stylesComponents.integer}>{integer}</Text>
+          <View style={stylesComponents.decimalContainer}>
+            <Text style={stylesComponents.decimal}>.{decimal}</Text>
+            <Text style={stylesComponents.degree}>°</Text>
           </View>
         </View>
       </LinearGradient>
