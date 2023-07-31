2b48d84b70779ec587bb4d678516853688b9476e
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-07-31 19:24:34 +0200

    Add thermostat pic, add home temperature container

--- components/SingleCard.js
+++ components/SingleCard.js
@@ -1,6 +1,8 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { stylesComponents } from './styles/styles';
 import { LinearGradient } from 'expo-linear-gradient';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import ThermostatIcon from '../assets/images/thermostatIcon.png';
 
 export const SingleCard = ({ value }) => {
   const [integer, decimal] = value.toString().split('.');
@@ -10,12 +12,23 @@ export const SingleCard = ({ value }) => {
         colors={['#c5f7d1', '#dffbca', '#f0f7dc']}
         style={stylesComponents.gradient}
       >
-        <View style={stylesComponents.container}>
-          <Text style={stylesComponents.integer}>{integer}</Text>
-          <View style={stylesComponents.decimalContainer}>
-            <Text style={stylesComponents.decimal}>.{decimal}</Text>
-            <Text style={stylesComponents.degree}>°</Text>
+        <View style={stylesComponents.tempStyles}>
+          <View style={stylesComponents.container}>
+            <Text style={stylesComponents.integer}>{integer}</Text>
+            <View style={stylesComponents.decimalContainer}>
+              <Text style={stylesComponents.decimal}>.{decimal}</Text>
+              <Text style={stylesComponents.degree}>°</Text>
+            </View>
           </View>
+          <View style={stylesComponents.homeTempContainer}>
+            <Icon name="home" size={30} color="#020" />
+            <Text style={stylesComponents.homeTempStyle}>
+              {integer}.{decimal}°
+            </Text>
+          </View>
+        </View>
+        <View>
+          <Image source={ThermostatIcon} style={stylesComponents.thermoIcon} />
         </View>
       </LinearGradient>
     </View>
